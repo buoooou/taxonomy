c96204c8401d443c7a5ebf23221054f24c66a40c
zhangkuo <769329489@qq.com>
2024-07-25 15:30:28 +0800

    注册登陆调整

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,13 +2,65 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
 import EmailProvider from "next-auth/providers/email"
 import GitHubProvider from "next-auth/providers/github"
-import { Client } from "postmark"
+import nodemailer from 'nodemailer';
 
 import { env } from "@/env.mjs"
 import { siteConfig } from "@/config/site"
 import { db } from "@/lib/db"
 
-const postmarkClient = new Client(env.POSTMARK_API_TOKEN)
+// const postmarkClient = new Client(env.POSTMARK_API_TOKEN)
+function html(params: { url: string, host: string, theme: Theme }) {
+  const { url, host, theme } = params
+
+  const escapedHost = host.replace(/\./g, "&#8203;.")
+
+  const brandColor = theme.brandColor || "#346df1"
+  const color = {
+    background: "#f9f9f9",
+    text: "#444",
+    mainBackground: "#fff",
+    buttonBackground: brandColor,
+    buttonBorder: brandColor,
+    buttonText: theme.buttonText || "#fff",
+  }
+
+  return `
+<body style="background: ${color.background};">
+  <table width="100%" border="0" cellspacing="20" cellpadding="0"
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center"
+        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        Sign in to <strong>${escapedHost}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
+                target="_blank"
+                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
+                in</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center"
+        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>
+`
+}
+
+/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
+function text({ url, host }: { url: string, host: string }) {
+  return `Sign in to ${host}\n${url}\n\n`
+}
 
 export const authOptions: NextAuthOptions = {
   // huh any! I know.
@@ -27,47 +79,73 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     EmailProvider({
-      from: env.SMTP_FROM,
-      sendVerificationRequest: async ({ identifier, url, provider }) => {
-        const user = await db.user.findUnique({
-          where: {
-            email: identifier,
-          },
-          select: {
-            emailVerified: true,
-          },
-        })
-
-        const templateId = user?.emailVerified
-          ? env.POSTMARK_SIGN_IN_TEMPLATE
-          : env.POSTMARK_ACTIVATION_TEMPLATE
-        if (!templateId) {
-          throw new Error("Missing template id")
+      from: env.EMAIL_FROM,
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASSWORD
         }
+      },
+      sendVerificationRequest: async ({ identifier, url, provider, theme }) => {
+        const { host } = new URL(url)
+        const transport = nodemailer.createTransport(provider.server);
 
-        const result = await postmarkClient.sendEmailWithTemplate({
-          TemplateId: parseInt(templateId),
-          To: identifier,
-          From: provider.from as string,
-          TemplateModel: {
-            action_url: url,
-            product_name: siteConfig.name,
-          },
-          Headers: [
-            {
-              // Set this to prevent Gmail from threading emails.
-              // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
-              Name: "X-Entity-Ref-ID",
-              Value: new Date().getTime() + "",
-            },
-          ],
-        })
+        const result = await transport.sendMail({
+          to: identifier,
+          from: provider.from,
+          subject: 'Your sign-in link for Buou',
+          text: text({ url, host }),
+          html: html({ url, host, theme }),
+        });
 
-        if (result.ErrorCode) {
-          throw new Error(result.Message)
+        const failed = result.rejected.concat(result.pending).filter(Boolean)
+        if (failed.length) {
+          throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`)
         }
       },
     }),
+    //   sendVerificationRequest: async ({ identifier, url, provider }) => {
+    //     const user = await db.user.findUnique({
+    //       where: {
+    //         email: identifier,
+    //       },
+    //       select: {
+    //         emailVerified: true,
+    //       },
+    //     })
+
+    //     const templateId = user?.emailVerified
+    //       ? env.POSTMARK_SIGN_IN_TEMPLATE
+    //       : env.POSTMARK_ACTIVATION_TEMPLATE
+    //     if (!templateId) {
+    //       throw new Error("Missing template id")
+    //     }
+
+    //     const result = await postmarkClient.sendEmailWithTemplate({
+    //       TemplateId: parseInt(templateId),
+    //       To: identifier,
+    //       From: provider.from as string,
+    //       TemplateModel: {
+    //         action_url: url,
+    //         product_name: siteConfig.name,
+    //       },
+    //       Headers: [
+    //         {
+    //           // Set this to prevent Gmail from threading emails.
+    //           // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
+    //           Name: "X-Entity-Ref-ID",
+    //           Value: new Date().getTime() + "",
+    //         },
+    //       ],
+    //     })
+
+    //     if (result.ErrorCode) {
+    //       throw new Error(result.Message)
+    //     }
+    //   },
+    // }),
   ],
   callbacks: {
     async session({ token, session }) {
